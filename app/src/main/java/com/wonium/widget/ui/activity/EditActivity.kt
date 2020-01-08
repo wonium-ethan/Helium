c5c1a1d639b1e5d6037cd3d90c98840fca25e04c
ethan <wonium@qq.com>
2020-01-08 20:30:13 +0800

    :bug: 修复demo 上引入的layout错误导致的Crash

--- app/src/main/java/com/wonium/widget/ui/activity/EditActivity.kt
+++ app/src/main/java/com/wonium/widget/ui/activity/EditActivity.kt
@@ -19,6 +19,6 @@ import com.wonium.widget.R
 class EditActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_colorful_line)
+        setContentView(R.layout.activity_edit)
     }
 }
